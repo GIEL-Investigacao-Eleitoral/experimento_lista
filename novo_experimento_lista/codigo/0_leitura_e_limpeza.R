585d0e358c18434b994c234c4deb0af85654f0bb
DATAUNIRIO <duttross@gmail.com>
2023-05-30 19:59:48 -0300

    new

--- novo_experimento_lista/codigo/0_leitura_e_limpeza.R
+++ novo_experimento_lista/codigo/0_leitura_e_limpeza.R
@@ -1,38 +1,33 @@
-
-#-----------------------------------------------------
-#                    Carregar
-#-----------------------------------------------------
+#EXPERIMENTO EM LISTA
+library(readxl)
 library(dplyr)
-library(haven)
-load("/home/steven/Área de Trabalho/CCJP/Borba/experimento_lista/BD_Covid_Faperj.RData")
-load("C:/Users/Hp/Google Drive (steven.ross@uniriotec.br)/CCJP/Borba/02 novembro de 2022 experimento de lista/banco/BD_Covid_Faperj.RData")
-#-----------------------------------------------------
+library(janitor)
+banco <- read_xlsx("C:/Users/Hp/Google Drive (steven.ross@uniriotec.br)/CCJP/Borba/29_05_2023/BDconcatenado2.xlsx") %>% clean_names()
+names(banco)
+table(banco$abaixo_ha_uma_lista_de_itens_com_criterios_que_os_eleitores_usam_para_escolher_o_seu_candidato_a_prefeito_a_com_quantos_desses_criterios_o_a_sr_a_concorda_nao_precisamos_saber_quais_estamos_interessados_apenas_na_quantidade_a_a_honestidade_e_o_preparo_para_o_cargo_do_candidato_b_as_propostas_de_governo_do_candidato_c_o_candidato_ser_a_indicacao_de_alguma_pessoa_de_confianca_do_eleitor_d_o_partido_politico_do_candidato_indique_abaixo_o_numero_de_itens_com_os_quais_o_a_sr_a_concorda_0_nenhum_1_2_3_ou_4)
+table(banco$controle)
+
+banco$resposta = banco$abaixo_ha_uma_lista_de_itens_com_criterios_que_os_eleitores_usam_para_escolher_o_seu_candidato_a_prefeito_a_com_quantos_desses_criterios_o_a_sr_a_concorda_nao_precisamos_saber_quais_estamos_interessados_apenas_na_quantidade_a_a_honestidade_e_o_preparo_para_o_cargo_do_candidato_b_as_propostas_de_governo_do_candidato_c_o_candidato_ser_a_indicacao_de_alguma_pessoa_de_confianca_do_eleitor_d_o_partido_politico_do_candidato_indique_abaixo_o_numero_de_itens_com_os_quais_o_a_sr_a_concorda_0_nenhum_1_2_3_ou_4
+table(banco$controle,banco$resposta)
+
+#------------------------------------------------------------------------------
 #           Variavel resposta
-#-----------------------------------------------------
+#------------------------------------------------------------------------------
 # a lista 1 tem 4 elementos
 # a lista 2 tem 5 elementos
-# a ultima variavel eh a combinacao linear das duas primeiras
-dicionario[31,] # lista 1
-dicionario[32,] # lista 2
-dicionario[33,] # lista 1 + lista 2
+class(banco$resposta)
+banco$resposta = as.numeric(banco$resposta)
+banco$controle = as.factor(banco$controle)
+banco = banco %>% filter(!is.na(resposta))
 
-max(banco$Q14_1, na.rm = T)
-max(banco$Q14_2, na.rm = T)
-mean(banco$Q14_1, na.rm = T)
-mean(banco$Q14_2, na.rm = T)
-
-sum(is.na(banco$Q14_1))
-sum(is.na(banco$Q14_2))
-sum(is.na(banco$Q14_3))
 
 #define standard error of mean function
 erro_padrao <- function(x) sd(x)/sqrt(length(x))
-tabela_medias = banco %>% group_by(experimento) %>% summarise(media=mean(Q14_3),erro_padrao=erro_padrao(Q14_3))
-tabela_medias$experimento <- haven::as_factor(tabela_medias$experimento)
+tabela_medias = banco %>% group_by(controle) %>% summarise(media=mean(resposta),erro_padrao=erro_padrao(resposta))
 
 # http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
 library(ggplot2)
-p<- ggplot(tabela_medias, aes(x=experimento, y=media, color=experimento)) + 
+p<- ggplot(tabela_medias, aes(x=controle, y=media, color=controle)) + 
   geom_pointrange(aes(ymin=media-1.96*erro_padrao, ymax=media+1.96*erro_padrao))
 # Finished line plot
 p+labs(x="Grupo", y = "Número médio de itens")+
@@ -42,14 +37,14 @@ p+labs(x="Grupo", y = "Número médio de itens")+
 # banco %>% group_by(experimento) %>% table(banco$Q14_3)
 library(janitor)
 library(flextable)
-banco %>% tabyl(Q14_3,experimento) %>%
+banco %>% tabyl(resposta,controle) %>%
   adorn_percentages("col") %>% round(2) %>% flextable()
 
 
 #----------------------------------------------------------------
 
 # preparando o banco para o pacote 'list'
-banco$experimento2 = ifelse(is.na(banco$Q14_2),FALSE,TRUE)
+banco$controle2 = ifelse(banco$controle=="0",FALSE,TRUE)
 banco$experimento3 = ifelse(banco$experimento2==T,1,0)
 banco %>% group_by(experimento2) %>% summarise(media=mean(Q14_3))
 banco %>% group_by(experimento3) %>% summarise(media=mean(Q14_3))
@@ -62,17 +57,20 @@ table(banco$experimento)
 library(list)
 
 # Conduct test with null hypothesis that there is no design effect
-teste <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = TRUE)
+teste <- ict.test(banco$resposta, banco$controle2, J = 4, gms = TRUE)
 print(teste)
 
-teste2 <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = FALSE)
+teste2 <- ict.test(banco$resposta, banco$controle2, J = 4, gms = FALSE)
 print(teste2)
 
 #gms	= A logical value indicating whether the generalized moment selection procedure should be used.
-teste <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = TRUE,pi.table	=TRUE,n.draws=50000)
+teste <- ict.test(banco$resposta, banco$controle2, J = 4, gms = TRUE,pi.table	=TRUE,n.draws=50000)
 print(teste)
-banco$Q14_3 <- as.numeric(banco$Q14_3)
 
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
 #----------------------------------------------------------------------
 # incluindo as covariaveis
 # sexo, idade, escolaridade, confianca na urna e ideologia
@@ -149,25 +147,25 @@ colnames(banco_reduzido) = c('y','treat','idade','sexo','ideologia','educa','con
 colnames(banco_reduzido) 
 # Calculate list experiment difference in means
 diff_medias <- ictreg(y ~ 1, data = banco_reduzido, 
-                     treat = "treat", J=4, method = "lm")
+                      treat = "treat", J=4, method = "lm")
 summary(diff_medias)
 
 aaa <- predict(diff_medias, newdata = as.data.frame(matrix(1, 1, 1)), se.fit = TRUE, 
-                                avg = TRUE)
+               avg = TRUE)
 aaa
 
 #----------------------------------------------------------------------------------------
 # Fit linear regression
 modelo_simples <- ictreg(y ~ idade + as.factor(sexo), data = banco_reduzido, 
-                     treat = "treat", J=4, method = "lm")
+                         treat = "treat", J=4, method = "lm")
 summary(modelo_simples)
 
 modelo_intermediario1 <- ictreg(y ~ idade + as.factor(sexo)+educa, data = banco_reduzido, 
-                     treat = "treat", J=4, method = "lm")
+                                treat = "treat", J=4, method = "lm")
 summary(modelo_intermediario1)
 
 modelo_intermediario2 <- ictreg(y ~ idade + as.factor(sexo)+educa+ideologia, data = banco_reduzido, 
-                     treat = "treat", J=4, method = "lm")
+                                treat = "treat", J=4, method = "lm")
 summary(modelo_intermediario2)
 
 lm.results <- ictreg(y ~ idade + as.factor(sexo)+ideologia+educa+confia_urna+importância_voto+regiao+renda, data = banco_reduzido, 

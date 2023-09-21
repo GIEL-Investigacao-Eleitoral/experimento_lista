1db0599f61e5ad96294635b1c55bf38603b5f546
DATAUNIRIO <duttross@gmail.com>
2023-09-21 12:13:53 -0300

    new

--- codigo/0_leitura_e_limpeza_ANPOCS_20_09_2023.R
+++ codigo/0_leitura_e_limpeza_ANPOCS_20_09_2023.R
@@ -1,20 +1,20 @@
 #EXPERIMENTO EM LISTA
 
-library(dplyr)
-library(haven)
-banco <- haven::read_spss("C:/Users/08451589707/Documents/GitHub/experimento_lista/anpocs/BD Covid-Faperj.sav")
-
-# criacao do dicionario de dados
-nomes<-names(banco)
-nomes <- data.frame(nomes)
-rotulos_perguntas<-lapply(banco, function(x) attributes(x)$label)
-rotulos_perguntas <- data.frame(matrix(unlist(rotulos_perguntas), nrow=length(rotulos_perguntas), byrow=F))
-rotulos_perguntas<-rotulos_perguntas[,1]
-dicionario<-data.frame(nomes,rotulos_perguntas)
-remove(nomes,rotulos_perguntas)
-names(dicionario) = c('variavel','descricao')
-
-save(banco,dicionario,file = "C:/Users/08451589707/Documents/GitHub/experimento_lista/anpocs/BD_Covid_Faperj_Anpocs.RData")
+# library(dplyr)
+# library(haven)
+# banco <- haven::read_spss("C:/Users/08451589707/Documents/GitHub/experimento_lista/anpocs/BD Covid-Faperj.sav")
+# 
+# # criacao do dicionario de dados
+# nomes<-names(banco)
+# nomes <- data.frame(nomes)
+# rotulos_perguntas<-lapply(banco, function(x) attributes(x)$label)
+# rotulos_perguntas <- data.frame(matrix(unlist(rotulos_perguntas), nrow=length(rotulos_perguntas), byrow=F))
+# rotulos_perguntas<-rotulos_perguntas[,1]
+# dicionario<-data.frame(nomes,rotulos_perguntas)
+# remove(nomes,rotulos_perguntas)
+# names(dicionario) = c('variavel','descricao')
+# 
+# save(banco,dicionario,file = "C:/Users/08451589707/Documents/GitHub/experimento_lista/anpocs/BD_Covid_Faperj_Anpocs.RData")
 
 
 #-----------------------------------------------------
@@ -45,13 +45,19 @@ sum(is.na(banco$Q14_3))
 
 table(banco$experimento)
 
-haven::print_labels(banco$FILTRO_PROFISSÃO)
-haven::print_labels(banco$FILIAÇÃO)
-haven::print_labels(banco$Q8)
-haven::print_labels(banco$v5)
-haven::print_labels(banco$Q28)
-haven::print_labels(banco$Q5)
+
+# Sociais
 haven::print_labels(banco$sexo)
+haven::print_labels(banco$faixaidade2)
+haven::print_labels(banco$Raça2)
+haven::print_labels(banco$escolaridade2)
+haven::print_labels(banco$renda2)
+haven::print_labels(banco$religião2)
+haven::print_labels(banco$região3)
 
-
-
+# Politicas
+haven::print_labels(banco$Q5)
+haven::print_labels(banco$Q6)
+haven::print_labels(banco$Q7)
+haven::print_labels(banco$Q8)
+ideologia2
--- codigo/1_grafico_ggplot_ANPOCS_20_09_2023.R
+++ codigo/1_grafico_ggplot_ANPOCS_20_09_2023.R
@@ -1,6 +1,92 @@
+
+#----------------------------------------------------------------------------------
+# Two-sample design
+#----------------------------------------------------------------------------------
 library(misty)
 
-  
+# Two-Sided 95% CI for y1 by group1
+# unknown population variances, unequal variance assumption
+# print results with 3 digits
+resultado1 = ci.mean.diff(Q14_3 ~ experimento, data = banco, digits = 3, na.omit = TRUE)
+resultado11 = data.frame(resultado1[["result"]][7],
+                         resultado1[["result"]][8],
+                         resultado1[["result"]][9])
+resultado11 = resultado11[2,]
+resultado11$categoria = '1 - global'
+resultado11
+
+library(ggplot2)
+ggplot(resultado11, aes(x=1, y=m.diff, group=1)) +
+  geom_pointrange(aes(ymin = low, ymax = upp), color='#00539C', fill='#00539C',linewidth = 2)+
+  geom_point(shape=21, size=5, fill='#00539C')+
+  geom_hline(yintercept = 0,linetype='dashed', col = 'red') + 
+  coord_flip()+
+  theme_classic()
+
+
+# Two-Sided 95% CI for y1, y2, and y3 by group1
+# unknown population variances, unequal variance assumption,
+# analysis by group2 separately
+haven::print_labels(banco$v2)
+resultado_sexo = ci.mean.diff(Q14_3 ~ experimento, data = banco, digits = 3, na.omit = TRUE, group =banco$v2)
+resultado_sexo1 = data.frame(resultado_sexo[["result"]][8],
+                             resultado_sexo[["result"]][9],
+                             resultado_sexo[["result"]][10])
+resultado_sexo1 = resultado_sexo1[c(2,4),]
+resultado_sexo1$categoria = c('2 - masculino','3 - feminino')
+
+
+resultado = resultado11 %>% add_row(resultado_sexo1)
+resultado$x = 1:3
+
+ggplot(resultado, aes(x=categoria, y=m.diff, group=categoria,color=categoria,fill=categoria)) +
+  geom_pointrange(aes(ymin = low, y=m.diff ,ymax = upp),linewidth = 2,shape=21, size=1.5)+
+  #geom_point(shape=21, size=5, fill='royalblue')+
+  geom_hline(yintercept = 0,linetype='dashed', col = 'red') +
+  scale_colour_manual(values = c("blue", "#FDE725FF","red")) +
+  scale_fill_manual(values = c("blue", "#FDE725FF","red")) +
+  coord_flip()+
+  theme_classic()+
+  labs(y='diferença entre as médias',x='',
+       subtitle = 'Gráfico 1 - Diferença no número médio de itens escolhidos do experimento em lista entre os grupos de tratamento e controle por variáveis sociais e políticas.
+Barras Horizontais representam o intervalo com 95% de confiança')
+
+# MESMO RESULTADO
+# Two-Sided 95% CI for y1, y2, and y3 by group1
+# unknown population variances, unequal variance assumption,split analysis by group2
+#ci.mean.diff(Q14_3 ~ experimento, data = banco, digits = 3, na.omit = TRUE, split=banco$v2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+
+
+library(misty)
+
+
 dat1 <- data.frame(group1 = c(1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
                               1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2),
                    group2 = c(1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2,
@@ -45,60 +131,4 @@ ci.mean.diff(cbind(x1, x2, x3) ~ group1, data = dat1,
              group = dat1$group2, split = dat1$group3)
 
 
-
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-# Two-sample design
-
-# Two-Sided 95% CI for y1 by group1
-# unknown population variances, unequal variance assumption
-# print results with 3 digits
-resultado1 = ci.mean.diff(Q14_3 ~ experimento, data = banco, digits = 3, na.omit = TRUE)
-resultado11 = data.frame(resultado1[["result"]][7],
-                         resultado1[["result"]][8],
-                         resultado1[["result"]][9])
-resultado11 = resultado11[2,]
-resultado11$categoria = 'global'
-
-library(ggplot2)
-ggplot(resultado11, aes(x=1, y=m.diff, group=1)) +
-  geom_pointrange(aes(ymin = low, ymax = upp), color='royalblue', fill='royalblue',linewidth = 2)+
-  geom_point(shape=21, size=5, fill='royalblue')+
-  geom_hline(yintercept = 0,linetype='dashed', col = 'red') + 
-  coord_flip()+
-  theme_classic()
-
-
-# Two-Sided 95% CI for y1, y2, and y3 by group1
-# unknown population variances, unequal variance assumption,
-# analysis by group2 separately
-haven::print_labels(banco$v2)
-resultado_sexo = ci.mean.diff(Q14_3 ~ experimento, data = banco, digits = 3, na.omit = TRUE, group =banco$v2)
-resultado_sexo1 = data.frame(resultado_sexo[["result"]][8],
-                             resultado_sexo[["result"]][9],
-                             resultado_sexo[["result"]][10])
-resultado_sexo1 = resultado_sexo1[c(2,4),]
-resultado_sexo1$categoria = c('masculino','feminino')
-
-
-resultado = resultado11 %>% add_row(resultado_sexo1)
-resultado$x = 1:3
-
-ggplot(resultado, aes(x=categoria, y=m.diff, group=1)) +
-  geom_pointrange(aes(ymin = low, ymax = upp), color='royalblue', fill='royalblue',linewidth = 2)+
-  geom_point(shape=21, size=5, fill='royalblue')+
-  geom_hline(yintercept = 0,linetype='dashed', col = 'red') + 
-  coord_flip()+
-  theme_classic()+
-  labs(y='diferença entre as médias',x='',
-       subtitle = 'Gráfico 1 - Diferença no número médio de itens escolhidos do experimento em lista entre os grupos de tratamento e controle por variáveis sociais e políticas.
-Barras Horizontais representam o intervalo com 95% de confiança')
-
-
-
-# MESMO RESULTADO
-# Two-Sided 95% CI for y1, y2, and y3 by group1
-# unknown population variances, unequal variance assumption,split analysis by group2
-#ci.mean.diff(Q14_3 ~ experimento, data = banco, digits = 3, na.omit = TRUE, split=banco$v2)
 

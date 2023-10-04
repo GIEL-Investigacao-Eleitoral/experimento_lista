df8d5ed6c1ad55e85887513f30beb911d28987bf
DATAUNIRIO <duttross@gmail.com>
2023-10-04 11:57:44 -0300

    Update 2_modelo_ANPOCS_20_09_2023.R

--- codigo/2_modelo_ANPOCS_20_09_2023.R
+++ codigo/2_modelo_ANPOCS_20_09_2023.R
@@ -1,30 +1,16 @@
 #-----------------------------------------------------
 #                    Carregar
 #-----------------------------------------------------
+
 library(dplyr)
 library(haven)
 load("C:/Users/08451589707/Documents/GitHub/experimento_lista/anpocs/BD_Covid_Faperj_Anpocs.RData")
 remove(dicionario)
 
-# Sociais
-haven::print_labels(banco$sexo)
-haven::print_labels(banco$faixaidade2)
-haven::print_labels(banco$Raça2)
-haven::print_labels(banco$escolaridade2)
-haven::print_labels(banco$renda2)
-haven::print_labels(banco$religião2)
-haven::print_labels(banco$região3)
-
-# Politicas
-haven::print_labels(banco$Q5)
-haven::print_labels(banco$Q6)
-haven::print_labels(banco$Q7)
-haven::print_labels(banco$Q8)
-haven::print_labels(banco$ideologia2)
-
 banco = banco %>% filter(ideologia2!=99)
 banco = banco %>% filter(Q8!=99)
 banco = banco %>% filter(Q7!=99)
+banco = banco %>% filter(Q7!=96)
 banco = banco %>% filter(Q5!=99)
 banco = banco %>% filter(ideologia2!=99)
 
@@ -34,7 +20,6 @@ banco = banco %>% filter(ideologia2!=99)
 #-----------------------------------------------------------------------
 # list = Statistical Methods for the Item Count Technique and List Experiment
 library(list)
-
 # preparando o banco para o pacote 'list'
 banco$experimento2 = ifelse(is.na(banco$Q14_2),FALSE,TRUE)
 banco %>% group_by(experimento2) %>% summarise(media=mean(Q14_3))
@@ -43,18 +28,17 @@ table(banco$experimento)
 
 
 # Conduct test with null hypothesis that there is no design effect
-teste <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = TRUE)
-print(teste)
-teste2 <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = FALSE)
-print(teste2)
+#teste <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = TRUE)
+#print(teste)
+#teste2 <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = FALSE)
+#print(teste2)
 #gms	= A logical value indicating whether the generalized moment selection procedure should be used.
-teste <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = TRUE,pi.table	=TRUE,n.draws=50000)
-print(teste)
+#teste <- ict.test(banco$Q14_3, banco$experimento2, J = 4, gms = TRUE,pi.table	=TRUE,n.draws=50000)
+#print(teste)
 
 #----------------------------------------------------------------------
 # incluindo as covariaveis
 #----------------------------------------------------------------------
-
 # Sociais
 haven::print_labels(banco$sexo)
 haven::print_labels(banco$faixaidade2)
@@ -64,6 +48,7 @@ haven::print_labels(banco$renda2)
 haven::print_labels(banco$religião2)
 haven::print_labels(banco$região3)
 
+
 # Politicas
 haven::print_labels(banco$Q5)
 haven::print_labels(banco$Q6)
@@ -71,6 +56,17 @@ haven::print_labels(banco$Q7)
 haven::print_labels(banco$Q8)
 haven::print_labels(banco$ideologia2)
 
+library(haven)
+haven::print_labels(banco$Q7)
+
+table(banco$Q7)
+# Contra = categoria de base
+# incluir a Q7 no gráfico
+
+attributes(banco$Q6) 
+attributes(banco$Q7) 
+attributes(banco$Q8) 
+  
 banco_reduzido = banco %>% select(Q14_3, experimento2,
                                   # Sociais
                                   sexo,faixaidade2,Raça2,
@@ -109,19 +105,19 @@ table(banco_reduzido$escolaridade2)
 banco_reduzido = banco_reduzido %>%
   mutate(
     renda2 = case_when(
-      renda2==1     ~ "Até 02",
-      renda2==2     ~ "02 a 05",
-      renda2==3     ~ "05 a 10",
-      renda2==4     ~ "Mais de 10"))
+      renda2==1     ~ "1. Até 02",
+      renda2==2     ~ "2. 02 a 05",
+      renda2==3     ~ "3. 05 a 10",
+      renda2==4     ~ "4. Mais de 10"))
 table(banco_reduzido$renda2)
 
 banco_reduzido = banco_reduzido %>%
   mutate(
     religião2 = case_when(
-      religião2==1     ~ "Católica",
-      religião2==2     ~ "Evangélica",
-      religião2==3     ~ "Outras religiões",
-      religião2==4     ~ "Sem religião"))
+      religião2==1     ~ "4 Católica",
+      religião2==2     ~ "1 Evangélica",
+      religião2==3     ~ "2 Outras religiões",
+      religião2==4     ~ "3 Sem religião"))
 table(banco_reduzido$religião2)
 banco_reduzido = banco_reduzido %>%
   mutate(
@@ -143,8 +139,8 @@ banco_reduzido = banco_reduzido %>%
   mutate(
     Q6 = case_when(
       Q6==1     ~ "A favor",
-      Q6==2     ~ "Contra",
-      Q6==3     ~ "Indiferente"))
+      Q6==2     ~ "Contra" )) #,
+      # Q6==3     ~ "Indiferente"))
 table(banco_reduzido$Q6)
 
 banco_reduzido = banco_reduzido %>%
@@ -165,10 +161,10 @@ table(banco_reduzido$Q8)
 banco_reduzido = banco_reduzido %>%
   mutate(
     ideologia2 = case_when(
-      ideologia2==1     ~ "Esquerda",
-      ideologia2==2     ~ "Centro",
-      ideologia2==5     ~ "Direita",
-      ideologia2==96     ~ "Nenhuma"))
+      ideologia2==1     ~ "4 Esquerda",
+      ideologia2==2     ~ "3 Centro",
+      ideologia2==5     ~ "2 Direita",
+      ideologia2==96     ~ "1 Nenhuma"))
 table(banco_reduzido$ideologia2)
 
 #banco_reduzido = na.omit(banco_reduzido)
@@ -176,7 +172,9 @@ table(banco_reduzido$ideologia2)
 # Não pode ser um tibble nem um spss
 banco_reduzido = data.frame(banco_reduzido)
 
-# Calculate list experiment difference in means
+
+#----------------------------------------------------------------------------------------
+Calculate list experiment difference in means
 diff_medias <- ictreg(Q14_3 ~ 1, data = banco_reduzido, 
                       treat = "experimento2", J=4, method = "lm")
 summary(diff_medias)
@@ -185,6 +183,7 @@ aaa <- predict(diff_medias, newdata = as.data.frame(matrix(1, 1, 1)), se.fit = T
                avg = TRUE)
 aaa
 remove(aaa,diff_medias,teste,teste2)
+
 #----------------------------------------------------------------------------------------
 # Fit linear regression
 modelo_social <- ictreg(Q14_3 ~ sexo + faixaidade2 +Raça2+
@@ -192,24 +191,6 @@ modelo_social <- ictreg(Q14_3 ~ sexo + faixaidade2 +Raça2+
                          data = banco_reduzido, 
                          treat = "experimento2", J=4, method = "lm")
 summary(modelo_social)
-
-#a estatística de teste para um teste Z tem a distribuição normal padrão sob a hipótese nula. 
-#Suponha que você execute um teste Z de duas caudas com um α de 0,05,
-#e obtenha uma estatística Z com base em seus dados de 2,5. 
-#Este valor Z corresponde a um valor-p de 0,0124. 
-#2*pnorm(2.5,lower.tail = FALSE)
-
-#Este valor é 2 vezes a probabilidade de que o teste estatístico não assume um valor igual a ou maior que o valor absoluto do referido valor realmente observado com base na sua amostra (sob H0). 
-#2* P(TS > |1,785|) = 2 * 0,0371 = 0,0742. Por isso o valor de p = 0,0742. 
-#2*pnorm(1.785,lower.tail = FALSE) # (1-pnorm(1.785))*2
-
-# 2*Pr(ET>|−2.14/1.08|) = 2*Pr(ET>|-1.981481|)= 2*Pr(ET>1.981481)
-# 2*pnorm(1.96,lower.tail = FALSE)
-
-#ET = 2.14/1.08
-#2*pnorm(ET,lower.tail = FALSE)
-#2*pt(ET, df=20, lower.tail = FALSE, log.p = FALSE)
-
 
 # Fit two-step non-linear least squares regression
 nls_modelo_social<- ictreg(Q14_3 ~ sexo + faixaidade2 +Raça2+
@@ -252,20 +233,20 @@ robust_modelo_social <- ictreg(
   treat = "experimento2", J=4, method = "ml", robust = TRUE)
 summary(robust_modelo_social)
 
-robust_modelo_social2 <- ictreg(
+modelo_social2_robusto <- ictreg(
   Q14_3 ~ sexo + faixaidade2 +Raça2+
     escolaridade2+renda2+religião2+região3,
   data = banco_reduzido, 
   treat = "experimento2",
   J = 4, method = "nls", robust = TRUE)
-summary(robust_modelo_social2)
+summary(modelo_social2_robusto)
 
 
 #--------------------------------------------------------------------------------
 # Modelo com as variáveis políticas
 #--------------------------------------------------------------------------------
 
-modelo_pol <- ictreg(Q14_3 ~ ideologia2 + Q5 + Q7+ Q8,
+modelo_pol <- ictreg(Q14_3 ~ ideologia2 + Q5 + Q6 + Q7+ Q8,
                         data = banco_reduzido, 
                         treat = "experimento2", J=4, method = "lm")
 summary(modelo_pol)
@@ -281,7 +262,7 @@ summary(robust_modelo_pol2)
 # Modelo completo
 #--------------------------------------------------------------------------------
 
-modelo_completo<- ictreg(Q14_3 ~ sexo + faixaidade2 +Raça2+ escolaridade2+renda2+religião2+região3 + ideologia2 + Q5 + Q7+ Q8,
+modelo_completo<- ictreg(Q14_3 ~ sexo + faixaidade2 +Raça2+ escolaridade2+renda2+religião2+região3 + ideologia2 + Q5  + Q6 + Q7+ Q8,
                      data = banco_reduzido, treat = "experimento2", J=4, method = "lm")
 summary(modelo_completo)
 
